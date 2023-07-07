d285c2458db0828318a50474a804a0d72731f2ae
yalinjob <Yalin1.arie@gmail.com>
2023-07-07 15:59:01 +0300

    update 0707

---
+++ goWithDynamoDb/pkg/handlers/handlers.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"goWithDynamoDb/user"
+
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var ErrorMethodNotAllowed = "method not allowed"
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
+}
+
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetachUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+
+		return apiResponse(http.StatusOK, result)
+	}
+
+	result, err := user.FetachUsers(tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
+
+}
